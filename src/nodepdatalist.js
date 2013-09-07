65d2aa57103915fe91b6d8a0894de24da80784e5
Steen Klingberg <steen@sk-ab.se>
2013-09-07 05:40:56 +0200

    v0.1.1

--- src/nodepdatalist.js
+++ src/nodepdatalist.js
@@ -45,7 +45,7 @@
 			'.nodep-dl-list {'+
 				'border: solid #043A6B 1px;'+
 				'position: absolute;'+
-				'max-height: 420px;'+
+				'max-height: 417px;'+
 				'z-index: 222;'+
 				'opacity: 0.8;'+
 				'background-color: #fff;'+
@@ -74,19 +74,47 @@
 				for (var i=0; i<n.length; i++) {
 					if (n[i] instanceof HTMLInputElement && n[i].hasAttribute('list')) {
 						noDepDatalist(n[i]);
+					} else
+					if (n[i] instanceof HTMLOptionElement) {
+						var par = n[i].parentNode;
+						console.log('ol added ' +  n[i].value + ' to ' + par.id);
+						var inp = document.querySelectorAll('input[list=' + par.id + ']');
+						for (var j=0; j<inp.length; j++) {
+							noDepDatalist(inp[j]);
+						}
+					} else
+					if (n[i] instanceof HTMLDataListElement) {
+						var obs = new MutationObserver(function (dlmut) {
+							dlmut.forEach(function (dlm) {
+								console.log(dlm.target + ' has muted');
+							})
+						})
+						obs.observe(n[i], {childList: true, attributes: true});
 					}
 				}
+
 			})
 		});
 		/* wait until dom is loaded */
 		document.addEventListener("DOMContentLoaded", function () {
 			var n = $('input[list]');
 			for (var i=0; i<n.length; i++) {
-				if (n[i] instanceof HTMLInputElement && n[i].hasAttribute('list')) {
+				if (n[i].hasAttribute('list')) {
 					noDepDatalist(n[i]);
 				}
 			}
-			observer.observe(document.body, {childList: true});
+			observer.observe(document.body, {childList: true, subtree: true});
+			var dl = $('datalist');
+			console.log('found ' + dl.length + ' datalists');
+			for (var i=0; i<dl.length; i++) {
+				var obs = new MutationObserver(function (dlmut) {
+					console.log(dlmut.target + ' has muted (*)');
+					//dlmut.forEach(function (dlm) {
+					//	console.log(dlm.target + ' has muted (*)');
+					//})
+				})
+				obs.observe(dl[i], {childList: true, attributes: true});
+			}
 		});
 		/* global go-away click */
 		window.addEventListener('click', function (evt) {
@@ -101,12 +129,19 @@
 
 	/* make the datalist popup */
 	function noDepDatalist (inp) {
+		console.log('makeing nodep datalist');
 		if (!inp.evt) {
 			inp.evt = function (ev, func) {
 	    		this.addEventListener(ev, func);
 				return this;
 			};
 		}
+		if (inp.optionlist) {
+			while (inp.optionlist.hasChildNodes()) {
+            	inp.optionlist.removeChild(inp.optionlist.firstChild);
+        	}
+        	inp.optionlist.parentNode.removeChild(inp.optionlist);
+        }
 		var	iid = inp.getAttribute('list')
 		 ,	list = $('#'+iid + ' option')
 		 ,	w = inp.clientWidth
@@ -114,27 +149,32 @@
 		 ,	t = inp.offsetTop + inp.clientHeight + 4
 		 ,	box = t$('div.nodep-dl-list', {style:'width: '+w+'px; left: '+l+'px; display: none; top:'+t+'px'})
 		 ,	list = $('#'+iid + ' option');
+		inp.optionlist = box;
 		for (var i=0; i<list.length; i++) {
-			var label = list[i].getAttribute('label') || list[i].textContent.trim() || list[i].value;
-			var itm = t$('div.nodep-dl-item.nodep-dl-visible', null, label)
-				.evt('click', function (evt) {
-					evt.preventDefault();
-					inp.value = evt.target.value;
-					box.style.display = 'none';
-				})
-				.evt('mouseover', function (evt) {
-					var sel = box.$('.nodep-dl-selected')[0];
-					if (sel) {
-						sel.classList.remove('nodep-dl-selected');
-					}
-					evt.target.classList.add('nodep-dl-selected');
-				})
-				.evt('mouseout', function (evt) {
-					evt.target.classList.remove('nodep-dl-selected');
-				})
-			;
-			itm.value = list[i].value;
-			box.appendChild(itm);
+			if (! list[i].hasAttribute('disabled')) {
+				var label = list[i].getAttribute('label') || list[i].textContent.trim() || list[i].value;
+				var itm = t$('div.nodep-dl-item.nodep-dl-visible', null, label)
+					.evt('click', function (evt) {
+						evt.preventDefault();
+						inp.value = evt.target.value;
+						box.style.display = 'none';
+					})
+					.evt('mouseover', function (evt) {
+						var sel = box.$('.nodep-dl-selected')[0];
+						if (sel) {
+							sel.classList.remove('nodep-dl-selected');
+						}
+						evt.target.classList.add('nodep-dl-selected');
+					})
+					.evt('mouseout', function (evt) {
+						evt.target.classList.remove('nodep-dl-selected');
+					})
+				;
+				itm.value = list[i].value;
+				box.appendChild(itm);
+				if (list[i].hasAttribute('selected'))
+					inp.value = list[i].value;
+			}
 		}
 		inp.parentNode.insertBefore(box, inp.nextSibling);
 
